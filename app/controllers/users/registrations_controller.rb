17fb78baa4342a8ca4768a8c60c630310f9498ab
Mayra Peguinho <may.peguinho0@gmail.com>
2024-12-01 16:09:01 +0000

    posibilidad de ver usuarios y de registrar usuarios

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :redirect_if_not_authenticated, only: [:new, :create]
+  skip_before_action :require_no_authentication, only: [:new, :create]
+  before_action :configure_sign_up_params, only: [:create]
+
+  private
+
+  def redirect_if_not_authenticated
+    unless user_signed_in?
+      redirect_to root_path, alert: 'Debes iniciar sesión para registrarte.'
+    end
+  end
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -41,9 +51,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:alias, :role_id, :entry_date, :tel])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
