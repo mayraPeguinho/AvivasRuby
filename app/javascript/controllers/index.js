798aefd15a746c247c236b3848e3ac951716f564
Mayra Peguinho <may.peguinho0@gmail.com>
2024-12-29 02:21:42 +0000

    descarto stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import Carousel from '@stimulus-components/carousel'
-
-
-application.register('carousel', Carousel)
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@stimulus-components/carousel", to: "@stimulus-components--carousel.js" # @6.0.0
-pin "swiper/bundle", to: "swiper--bundle.js" # @11.1.15
